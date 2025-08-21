25d796e19fd5e9a78d2ac3fcda10817808cdb2cf
businapavel <businapavel@gmail.com>
2025-08-22 00:01:55 +0200

    photo motion

--- src/app/components/photo/photo.tsx
+++ src/app/components/photo/photo.tsx
@@ -2,16 +2,23 @@
 
 import React from 'react';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 export const Photo = () => {
     return (
-        <Image
-            src="/me.png"
-            alt="Profile"
-            width={256}
-            height={256}
-            priority
-            className="w-40 h-40 sm:w-64 sm:h-64 object-cover rounded-full shadow-xl"
-        />
+        <motion.div
+            initial={{ opacity: 0, y: -30, scale: 0.7 }}
+            whileInView={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ duration: 0.5, ease: 'easeInOut' }}
+        >
+            <Image
+                src="/me.png"
+                alt="Profile"
+                width={256}
+                height={256}
+                priority
+                className="w-40 h-40 sm:w-56 sm:h-56 object-cover rounded-full shadow-xl"
+            />
+        </motion.div>
     );
 };
