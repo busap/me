9089eb6d6622776cd7533d575172e01aba77ba19
businapavel <businapavel@gmail.com>
2025-08-21 20:22:43 +0200

    role

---
+++ src/app/components/role/role.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import React from 'react';
+import { motion } from 'framer-motion';
+
+export type RoleProps = {
+    mainLeft?: string;
+    mainRight?: string;
+    leftAdornmentText?: string;
+    rightAdornmentText?: string;
+    rightIconAdornment?: React.ComponentType<{
+        size?: number;
+        className?: string;
+    }>;
+    className?: string;
+};
+
+export const Role: React.FC<RoleProps> = ({
+    mainLeft = '',
+    mainRight = '',
+    leftAdornmentText,
+    rightAdornmentText,
+    rightIconAdornment,
+    className
+}) => {
+    const wrapperBaseCls =
+        'flex justify-center text-2xl sm:text-4xl whitespace-break-spaces flex-wrap gap-2';
+    const textCls =
+        'text-xl sm:text-3xl font-extrabold tracking-wide text-slate-600';
+    const coloredCls = 'text-xl sm:text-3xl text-teal-600 font-bold tracking-wide';
+    const wrapperCls = `${wrapperBaseCls} ${className ? className : ''}`;
+
+    const renderMainLeft = () =>
+        mainLeft ? (
+            <motion.span
+                className={textCls}
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3, delay: 0.3 }}
+            >
+                {mainLeft}
+            </motion.span>
+        ) : null;
+
+    const renderMainRight = () =>
+        mainRight ? (
+            <motion.span
+                className={textCls}
+                initial={{ opacity: 0, y: -20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3, delay: 0.5 }}
+            >
+                {mainRight}
+            </motion.span>
+        ) : null;
+
+    const renderColoredText = (text: string, delay: number) => (
+        <motion.span
+            className={coloredCls}
+            initial={{ x: 20, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.2, delay }}
+        >
+            {text}
+        </motion.span>
+    );
+
+    const renderRightIconAdornment = (delay: number) => {
+        if (!rightIconAdornment) return null;
+        const Icon = rightIconAdornment;
+        return (
+            <motion.span
+                className={coloredCls}
+                initial={{ x: 20, opacity: 0 }}
+                whileInView={{ x: 0, opacity: 1 }}
+                transition={{ duration: 0.2, delay }}
+            >
+                <Icon size={24} className="align-middle" />
+            </motion.span>
+        );
+    };
+
+    return (
+        <div className={wrapperCls}>
+            <div className={'flex justify-center items-center gap-2'}>
+                {rightIconAdornment && renderRightIconAdornment(1.0)}
+                {leftAdornmentText
+                    ? renderColoredText(leftAdornmentText, 0.8)
+                    : null}
+                {renderMainLeft()}
+                {rightAdornmentText
+                    ? renderColoredText(rightAdornmentText, 0.8)
+                    : null}
+            </div>
+            {renderMainRight()}
+        </div>
+    );
+};
