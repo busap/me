32b53eb98e2d4b76bfa86952ef0fdff523be4b23
businapavel <businapavel@gmail.com>
2025-08-23 17:58:34 +0200

    image split

--- src/app/components/photo/photo.tsx
+++ src/app/components/photo/photo.tsx
@@ -10,15 +10,30 @@ export const Photo = () => {
             initial={{ opacity: 0, y: -30, scale: 0.7 }}
             whileInView={{ opacity: 1, y: 0, scale: 1 }}
             transition={{ duration: 0.5, ease: 'easeInOut' }}
+            className="relative w-40 h-40 sm:w-56 sm:h-56 rounded-full shadow-xl"
         >
-            <Image
-                src="/me.png"
-                alt="Profile"
-                width={256}
-                height={256}
-                priority
-                className="w-40 h-40 sm:w-56 sm:h-56 object-cover rounded-full shadow-xl"
-            />
+            <div className="absolute inset-0 flex">
+                <div className="w-1/2">
+                    <Image
+                        src="/me-dev.png"
+                        alt="Profile Left"
+                        width={256}
+                        height={256}
+                        priority
+                        className="h-full object-cover object-left"
+                    />
+                </div>
+                <div className="w-1/2">
+                    <Image
+                        src="/me-travel.png"
+                        alt="Profile Right"
+                        width={256}
+                        height={256}
+                        priority
+                        className="h-full object-cover object-right"
+                    />
+                </div>
+            </div>
         </motion.div>
     );
 };
