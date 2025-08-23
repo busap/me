2e4707ac13b8a88ddaa5352e44b7dd773668f8c8
businapavel <businapavel@gmail.com>
2025-08-23 19:27:26 +0200

    image split ratio

--- src/app/components/photo/photo.tsx
+++ src/app/components/photo/photo.tsx
@@ -1,10 +1,41 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 
 export const Photo = () => {
+    const [scrollRatio, setScrollRatio] = useState(0.5);
+
+    useEffect(() => {
+        const handleMouseMove = (event: MouseEvent) => {
+            const screenWidth = window.innerWidth;
+            const cursorX = event.clientX;
+
+            const ratio = cursorX / screenWidth;
+
+            let adjustedRatio;
+            if (ratio <= 0.25) {
+                adjustedRatio = 0;
+            } else if (ratio >= 0.75) {
+                adjustedRatio = 1;
+            } else {
+                adjustedRatio = (ratio - 0.25) / 0.5;
+            }
+
+            setScrollRatio(adjustedRatio);
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
+    const leftWidth = `${(1 - scrollRatio) * 100}%`;
+    const rightWidth = `${scrollRatio * 100}%`;
+
     return (
         <motion.div
             initial={{ opacity: 0, y: -30, scale: 0.7 }}
@@ -13,24 +44,24 @@ export const Photo = () => {
             className="relative w-40 h-40 sm:w-56 sm:h-56 rounded-full shadow-xl"
         >
             <div className="absolute inset-0 flex">
-                <div className="w-1/2">
+                <div style={{ width: leftWidth }}>
                     <Image
                         src="/me-dev.png"
                         alt="Profile Left"
                         width={256}
                         height={256}
                         priority
-                        className="h-full object-cover object-left"
+                        className="h-full w-full object-cover object-left"
                     />
                 </div>
-                <div className="w-1/2">
+                <div style={{ width: rightWidth }}>
                     <Image
                         src="/me-travel.png"
                         alt="Profile Right"
                         width={256}
                         height={256}
                         priority
-                        className="h-full object-cover object-right"
+                        className="h-full w-full object-cover object-right"
                     />
                 </div>
             </div>
