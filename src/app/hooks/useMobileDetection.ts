560edab461ad4f3566610d255b0b3d4c2b7d0425
businapavel <businapavel@gmail.com>
2025-09-09 18:43:40 +0200

    use mobile detection

---
+++ src/app/hooks/useMobileDetection.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+export const useMobileDetection = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkMobile = () => {
+            const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+            const isSmallScreen = window.innerWidth < 768;
+            const isMobileUserAgent = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+            setIsMobile(isTouchDevice && (isSmallScreen || isMobileUserAgent));
+        };
+
+        checkMobile();
+
+        window.addEventListener('resize', checkMobile);
+
+        return () => {
+            window.removeEventListener('resize', checkMobile);
+        };
+    }, []);
+
+    return isMobile;
+};
