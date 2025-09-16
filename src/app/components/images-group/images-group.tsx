d52f4c53e1584ad72769269ae329c50ab6235fba
businapavel <businapavel@gmail.com>
2025-09-16 23:24:57 +0200

    images group (remove filter, update mobile)

--- src/app/components/images-group/images-group.tsx
+++ src/app/components/images-group/images-group.tsx
@@ -4,19 +4,13 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { useSplitRatio } from '@/src/app/hooks/useSplitRatio';
 import {AnimatedImage} from "@/src/app/components/animated-image/animated-image";
+import {useMobileDetection} from "@/src/app/hooks/useMobileDetection";
 
 export const ImagesGroup = () => {
+    const isMobile = useMobileDetection();
     const splitRatio = useSplitRatio(0.25, 0.75);
-    const leftWidth = `${(1 - splitRatio) * 100}%`;
-    const rightWidth = `${splitRatio * 100}%`;
-    const leftWidthPercent = (1 - splitRatio) * 100;
-    const rightWidthPercent = splitRatio * 100;
-    const leftFilter = leftWidthPercent < 50
-        ? `saturate(${(leftWidthPercent / 50) * 0.8})`
-        : `none`
-    const rightFilter = rightWidthPercent < 50
-        ? `saturate(${(rightWidthPercent / 50) * 0.8})`
-        : `none`;
+    const leftWidth = isMobile ? "50%" : `${(1 - splitRatio) * 100}%`;
+    const rightWidth = isMobile ? "50%" : `${splitRatio * 100}%`;
     const horizontalOffset = (splitRatio - 0.5) * 30;
 
     const renderProfilePictures = () => (
@@ -30,7 +24,6 @@ export const ImagesGroup = () => {
                 }}
                 containerClassName={"relative"}
                 className="h-full w-full object-cover object-left"
-                style={{ filter: leftFilter }}
                 animation={{ width: leftWidth }}
                 priority
                 quality={100}
@@ -44,7 +37,6 @@ export const ImagesGroup = () => {
                 }}
                 containerClassName={"relative"}
                 className="h-full w-full object-cover object-right"
-                style={{ filter: rightFilter }}
                 animation={{ width: rightWidth }}
                 priority
                 quality={100}
