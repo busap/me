d67ad1978871bcfe641904524a9de5a89b8deec1
businapavel <businapavel@gmail.com>
2025-10-22 19:16:34 +0200

    images group changes

--- src/app/components/images-group/images-group.tsx
+++ src/app/components/images-group/images-group.tsx
@@ -5,6 +5,7 @@ import { motion } from 'framer-motion';
 import { useSplitRatio } from '@/src/app/hooks/useSplitRatio';
 import { AnimatedImage } from '@/src/app/components/animated-image/animated-image';
 import { useMobileDetection } from '@/src/app/hooks/useMobileDetection';
+import { BackgroundCode } from '@/src/app/components/animated-background/background-code';
 
 export const ImagesGroup = () => {
     const isMobile = useMobileDetection();
@@ -14,45 +15,71 @@ export const ImagesGroup = () => {
     const horizontalOffset = (splitRatio - 0.5) * 30;
 
     const renderProfilePictures = () => (
-        <div className="absolute inset-0 flex z-10">
-            <AnimatedImage
-                src="/me-dev.png"
-                alt="Profile Left"
-                imageSize={{
-                    width: 294,
-                    height: 294,
-                }}
-                containerClassName={'relative'}
-                className="h-full w-full object-cover object-left"
-                animation={{ width: leftWidth }}
-                priority
-                quality={100}
-            />
-            <AnimatedImage
-                src="/me-travel.png"
-                alt="Profile Right"
-                imageSize={{
-                    width: 294,
-                    height: 294,
-                }}
-                containerClassName={'relative'}
-                className="h-full w-full object-cover object-right"
-                animation={{ width: rightWidth }}
-                priority
-                quality={100}
-            />
-        </div>
-    );
-
-    return (
         <motion.div
-            initial={{ opacity: 0, y: -30, scale: 0.7 }}
-            whileInView={{ opacity: 1, y: 0, scale: 1 }}
+            initial={{ opacity: 0, scale: 0.7 }}
+            whileInView={{ opacity: 1, scale: 1 }}
             animate={{ x: horizontalOffset }}
             transition={{ duration: 0.5, ease: 'linear' }}
-            className="relative w-46 h-46 sm:w-60 sm:h-60 rounded-full shadow-xl"
+            className="relative z-10 w-[150px] h-[150px] sm:w-[200px] sm:h-[200px] rounded-full shadow-xl"
         >
-            {renderProfilePictures()}
+            <div className="absolute inset-0 flex z-10">
+                <AnimatedImage
+                    src="/me-dev.png"
+                    alt="Profile Left"
+                    imageSize={{
+                        width: 200,
+                        height: 200,
+                    }}
+                    containerClassName={'relative'}
+                    className="h-full w-full object-cover object-left"
+                    animation={{ width: leftWidth }}
+                    priority
+                    quality={100}
+                />
+                <AnimatedImage
+                    src="/me-travel.png"
+                    alt="Profile Right"
+                    imageSize={{
+                        width: 200,
+                        height: 200,
+                    }}
+                    containerClassName={'relative'}
+                    className="h-full w-full object-cover object-right"
+                    animation={{ width: rightWidth }}
+                    priority
+                    quality={100}
+                />
+            </div>
         </motion.div>
+    );
+
+    const renderBackground = () => {
+        return (
+            <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                animate={{ x: horizontalOffset * 0.5 }}
+                transition={{ duration: 0.5, ease: 'linear' }}
+                className="flex absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg overflow-hidden"
+                style={{
+                    width: isMobile ? 160 : 450,
+                    height: isMobile ? 160 : 250,
+                }}
+            >
+                <div className="w-[50%] h-full">
+                    <BackgroundCode />
+                </div>
+                <div className="w-[50%] h-full">
+                    <BackgroundCode />
+                </div>
+            </motion.div>
+        );
+    };
+
+    return (
+        <div className="relative">
+            {renderProfilePictures()}
+            {renderBackground()}
+        </div>
     );
 };
