b4c6d8c870777e1a8f3878ca5c8e7dc5e091e3bc
businapavel <businapavel@gmail.com>
2025-10-30 21:28:29 +0100

    images group clean

--- src/app/components/images-group/images-group.tsx
+++ src/app/components/images-group/images-group.tsx
@@ -5,7 +5,6 @@ import { motion } from 'framer-motion';
 import { useSplitRatio } from '@/src/app/hooks/useSplitRatio';
 import { AnimatedImage } from '@/src/app/components/animated-image/animated-image';
 import { useMobileDetection } from '@/src/app/hooks/useMobileDetection';
-import { BackgroundCode } from '@/src/app/components/animated-background/background-code';
 
 export const ImagesGroup = () => {
     const isMobile = useMobileDetection();
@@ -14,7 +13,7 @@ export const ImagesGroup = () => {
     const rightWidth = isMobile ? '50%' : `${splitRatio * 100}%`;
     const horizontalOffset = (splitRatio - 0.5) * 30;
 
-    const renderProfilePictures = () => (
+    return (
         <motion.div
             initial={{ opacity: 0, scale: 0.7 }}
             whileInView={{ opacity: 1, scale: 1 }}
@@ -51,35 +50,5 @@ export const ImagesGroup = () => {
                 />
             </div>
         </motion.div>
-    );
-
-    const renderBackground = () => {
-        return (
-            <motion.div
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                animate={{ x: horizontalOffset * 0.5 }}
-                transition={{ duration: 0.5, ease: 'linear' }}
-                className="flex absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg overflow-hidden"
-                style={{
-                    width: isMobile ? 160 : 450,
-                    height: isMobile ? 160 : 250,
-                }}
-            >
-                <div className="w-[50%] h-full">
-                    <BackgroundCode />
-                </div>
-                <div className="w-[50%] h-full">
-                    <BackgroundCode />
-                </div>
-            </motion.div>
-        );
-    };
-
-    return (
-        <div className="relative">
-            {renderProfilePictures()}
-            {renderBackground()}
-        </div>
     );
 };
